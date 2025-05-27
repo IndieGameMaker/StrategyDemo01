4fe776e595962dc380354c4b3c8dc15fd6e024cd
Lee JaeHyun <myleje@gmail.com>
2025-05-27 16:28:06 +0900

    .

---
+++ Assets/02_Scripts/WeaponStrategyBase.cs
@@ -0,0 +1,10 @@
+public class WeaponStrategyBase : IWeaponStrategy
+{
+    public float damage { get; set; }
+    public float range { get; set; }
+    
+    public void Attack(ITarget target)
+    {
+        
+    }
+}
